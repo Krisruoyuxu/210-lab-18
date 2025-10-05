c183d510c910bb078a58d38442fc38e8408e23ce
dickxumamba <xuruoyu27@gmail.com>
2025-10-05 20:24:21 +0800

    updated

--- lab18.cpp
+++ lab18.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 using namespace std;
 
+struct Node {
+    double rating;      // 0–5
+    string comment;     // user’s review text
+    Node* next;         // pointer to next node
+};
+
+
+
 int main(){
-    
-    
-    
+    int choice;
+    // prompt the user which mode to use, new nodes at the head or the tail of the linked list.
+    cout << "Which linked list method should we use?" << endl; 
+    cout << "[1] New nodes are added at the head of the linked list" <<endl;
+    cout << "[2] New nodes are added at the tail of the linked list" <<endl;
+    cin >> choice;
+    cout << (choice == 1 ? "Choice: 1" : "Choice: 2") << endl;
     return 0;
 }
