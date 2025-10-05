b6cbcaf39ac4a9a36af4c23fa6ec4c47114b530e
dickxumamba <xuruoyu27@gmail.com>
2025-10-05 20:35:45 +0800

    updated

--- lab18.cpp
+++ lab18.cpp
@@ -7,15 +7,21 @@ struct Node {
     Node* next;         // pointer to next node
 };
 
-
+void rating(Node& node, double rate);
+void comment(Node& node, string comment);
+void enter_review(string result);
 
 int main(){
+    
     int choice;
-    // prompt the user which mode to use, new nodes at the head or the tail of the linked list.
+    //First prompt the user which mode to use, new nodes at the head or the tail of the linked list.
     cout << "Which linked list method should we use?" << endl; 
     cout << "[1] New nodes are added at the head of the linked list" <<endl;
     cout << "[2] New nodes are added at the tail of the linked list" <<endl;
     cin >> choice;
     cout << (choice == 1 ? "Choice: 1" : "Choice: 2") << endl;
+
+    Node *head = nullptr;
+
     return 0;
 }
